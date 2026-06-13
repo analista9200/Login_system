4b26eed2509991dbd52d9fbb80c5e9281233ec93
Francysberto <analista9200@gmail.com>
2026-06-12 21:25:06 -0300

    New css updated

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, './frontend')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/index.html'));
+  res.sendFile(path.join(__dirname, './frontend/index.html'));
 });
 
 app.listen(PORT, () => {
