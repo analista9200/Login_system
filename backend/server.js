a2b149fc31095bd88ccdbcca7af92d0fa373ef20
Francysberto <analista9200@gmail.com>
2026-08-18 21:15:08 -0300

    atualização do code

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get('/', (req,res)=>{
 
-res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 
 })
 
--- server.js
+++ server.js
@@ -1,24 +1,19 @@
-const express = require('express');
-const cors = require ('cors');
-const path = require('path');
-// importando rotas do sistema //
-const authRouts = require ('./routes/auth.js');
-
+const express = require("express");
+const cors = require("cors");
+const path = require('path')
+const authRoutes = require("./backend//routes/auth");
 const app = express();
-const PORT = 3000;
 
-// Use Middlewares Principais //
-app.use (cors())
-app.use (express.json) // Escreve e Ler requisições no formato json //
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend")));
 
-app.use(express.static(path.join(__dirname, './frontend')));
+app.get('/', (req,res)=>{
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+})
 
-app.use("/api", authRouts)
+app.use("/api" , authRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './frontend/index.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor Rodando em http://localhost:${PORT}`);
+app.listen(3001, () => {
+  console.log("Servidor rodando em http://localhost:3001");
 });
